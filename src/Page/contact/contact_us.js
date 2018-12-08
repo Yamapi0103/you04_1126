6cbdf0fb1766df8c1917f08a995f6dcc67c9514d
mayo <chienduan18@gmail.com>
2018-12-08 15:47:25 +0800

    修改聯絡我們的表單格式

--- src/Page/contact/contact_us.js
+++ src/Page/contact/contact_us.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import swal from 'sweetalert';
 // import { Link } from 'react-router-dom';
 import './contact_us.scss';
@@ -28,12 +30,12 @@ class contact_us extends Component {
 
     addHandler = (evt) => {
         fetch('http://localhost:3000/api/contact_us', {
-            method: 'POST',
-            body: JSON.stringify(this.state),
-            headers: new Headers({
-                'content-type': 'application/json'
+                method: 'POST',
+                body: JSON.stringify(this.state),
+                headers: new Headers({
+                    'content-type': 'application/json'
+                })
             })
-        })
             .then(res => res.json())
             .then(data => {
                 swal(data.message, "成功送出");
@@ -47,36 +49,92 @@ class contact_us extends Component {
 
 
     render() {
-        return (
-            <React.Fragment>
-                <form className="publish_container">
-                    <h3>聯絡我們</h3>
-                    <br />
-                    <div className="form-group">
-                        <label>如何稱呼您：</label>
-                        <input type="text" id="cu_name" value={this.state.cu_name} onChange={this.change} className="form-control" />
-                    </div>
-                    <div className="form-group">
-                        <label>您是網紅 / 廠商？</label>
-                        <input type="text" id="cu_usertype" value={this.state.cu_usertype} onChange={this.change} className="form-control" />
-                    </div>
-                    <div className="form-group">
-                        <label>您的問題/建議：</label>
-                        <textarea type="textarea" id="cu_content" value={this.state.cu_content} onChange={this.change} className="form-control" />
-                    </div>
-                    <div className="form-group">
-                        <label>您的聯絡 Email：</label>
-                        <input type="email" id="cu_email" value={this.state.cu_email} onChange={this.change} className="form-control" />
-                    </div>
-                    <br />
-                    <div className="cu_btn">
-                        <button className="btn"
-                            onClick={this.addHandler}>送出
-                        </button>
-                    </div>
+        return ( <
+            React.Fragment >
+            <
+            form className = "publish_container" >
+            <
+            h3 > 聯絡我們 < /h3> <
+            br / >
+            <
+            div className = "form-group" >
+            <
+            label > 如何稱呼您： < /label> <
+            input type = "text"
+            id = "cu_name"
+            value = {
+                this.state.cu_name
+            }
+            onChange = {
+                this.change
+            }
+            className = "form-control" / >
+            <
+            /div> {
+                /* <div className="form-group">
+                                        <label>您是網紅 / 廠商？</label>
+                                        <input type="text" id="cu_usertype" value={this.state.cu_usertype} onChange={this.change} className="form-control" />
+                                    </div> */
+            } <
+            div className = "form-group" >
+            <
+            label > 您的身分是？ < /label> <
+            select className = "form-control"
+            id = "cu_usertype"
+            value = {
+                this.state.cu_usertype
+            }
+            onChange = {
+                this.change
+            } >
+            <
+            option > 網紅 < /option> <
+            option > 廠商 < /option> <
+            option > 其他 < /option> <
+            /select> <
+            /div> <
+            div className = "form-group" >
+            <
+            label > 您的問題 / 建議： < /label> <
+            textarea type = "textarea"
+            id = "cu_content"
+            value = {
+                this.state.cu_content
+            }
+            onChange = {
+                this.change
+            }
+            className = "form-control" / >
+            <
+            /div> <
+            div className = "form-group" >
+            <
+            label > 您的聯絡 Email： < /label> <
+            input type = "email"
+            id = "cu_email"
+            value = {
+                this.state.cu_email
+            }
+            onChange = {
+                this.change
+            }
+            className = "form-control" / >
+            <
+            /div> <
+            br / >
+            <
+            div className = "cu_btn" >
+            <
+            button className = "btn"
+            onClick = {
+                this.addHandler
+            } > 送出 <
+            /button> <
+            /div>
 
-                </form>
-            </React.Fragment>
+            <
+            /form> <
+            /React.Fragment>
         )
     }
 }
