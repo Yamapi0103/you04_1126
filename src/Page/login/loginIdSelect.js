0ca6524095e7682a7fb7787a16cea40ab3234831
Ethan <pcomwilly@hotmail.com>
2018-12-11 18:54:41 +0800

    微調

--- src/Page/login/loginIdSelect.js
+++ src/Page/login/loginIdSelect.js
@@ -15,7 +15,7 @@ class loginIdSelect extends Component{
                 <div className="register_background">
                     <div className="register_container_a"> 
                         <div className="register_container_a_text">
-                            <h4>請選擇你的身份</h4>
+                            <h4>會員登入</h4>
                         </div>
                         <div className="register_container_a_btn">
                             <Link to="/login/bs">我是廠商</Link>
--- src/Page/register/Register.js
+++ src/Page/register/Register.js
@@ -21,7 +21,7 @@ class Register extends Component{
                 <div className="register_background">
                     <div className="register_container_a"> 
                         <div className="register_container_a_text">
-                            <h4>請選擇你的身份</h4>
+                            <h4>會員註冊</h4>
                         </div>
                         <div className="register_container_a_btn">
                             <Link to="/register/CompanyRegister">我是廠商</Link>
