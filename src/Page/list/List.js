99d01ae9fc85eb22bd5493261f3155204acce8c2
Ethan <pcomwilly@hotmail.com>
2018-12-15 13:40:56 +0800

    list頁 RWD半成品

--- src/Page/list/List.js
+++ src/Page/list/List.js
@@ -161,6 +161,74 @@ class List extends Component{
                 <div className="list_section1">
                     <div className="list_section1_searchbar_container">
                         <SearchBar />
+                        <div class="filter">
+                            <h2>網紅需求數</h2>
+                            <ul>
+                                <li>
+                                    <input type="radio" id="people-1" name="selector" className="people-filter" data-dropdown-value="1" />
+                                    <label for="people-1">需求人數：1</label>
+                                    <div class="check"></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="people-2" name="selector" className="people-filter" data-dropdown-value="2" />
+                                    <label for="people-2">需求人數：2</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="people-3" name="selector" className="people-filter" data-dropdown-value="3" />
+                                    <label for="people-3">需求人數：3</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="people-4" name="selector" className="people-filter" data-dropdown-value="3人以上" />
+                                    <label for="people-4">需求人數：3人以上</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                            </ul>
+                            
+                            <h2 className="mt-3">網紅粉絲需求數</h2>
+                            <ul>
+                                <li>
+                                    <input type="radio" id="fans-1" name="selector" className="fans-filter" data-dropdown-value="100~500" />
+                                    <label for="fans-1">微量粉絲數</label>
+                                    <div class="check"></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="fans-2" name="selector" className="fans-filter" data-dropdown-value="500~1000" />
+                                    <label for="fans-2">輕量粉絲數</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="fans-3" name="selector" className="fans-filter" data-dropdown-value="1000~2000" />
+                                    <label for="fans-3">中量級粉絲數</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="fans-4" name="selector" className="fans-filter" data-dropdown-value="2000~5000" />
+                                    <label for="fans-4">重量級粉絲數</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                            </ul>
+                            
+                            <h2 className="mt-3">案子薪水需求</h2>
+                            <ul>
+                                <li>
+                                    <input type="radio" id="pay-1" name="selector" className="pay-filter" data-dropdown-value="1,000-3,000元" />
+                                    <label for="pay-1">要求低薪</label>
+                                    <div class="check"></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="pay-2" name="selector" className="pay-filter" data-dropdown-value="3,000-5,000元" />
+                                    <label for="pay-2">剛好就好</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="pay-3" name="selector" className="pay-filter" data-dropdown-value="10,000-50,000元" />
+                                    <label for="pay-3">我要高薪</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                     
                 </div>
@@ -183,74 +251,74 @@ class List extends Component{
                         </div>
                     </div>
                     <div className="list_section2_option_container">
-                    <div class="filter">
-                        <h2>網紅需求數</h2>
-                        <ul>
-                            <li>
-                                <input type="radio" id="people-1" name="selector" className="people-filter" data-dropdown-value="1" />
-                                <label for="people-1">需求人數：1</label>
-                                <div class="check"></div>
-                            </li>
-                            <li>
-                                <input type="radio" id="people-2" name="selector" className="people-filter" data-dropdown-value="2" />
-                                <label for="people-2">需求人數：2</label>
-                                <div class="check"><div class="inside"></div></div>
-                            </li>
-                            <li>
-                                <input type="radio" id="people-3" name="selector" className="people-filter" data-dropdown-value="3" />
-                                <label for="people-3">需求人數：3</label>
-                                <div class="check"><div class="inside"></div></div>
-                            </li>
-                            <li>
-                                <input type="radio" id="people-4" name="selector" className="people-filter" data-dropdown-value="3人以上" />
-                                <label for="people-4">需求人數：3人以上</label>
-                                <div class="check"><div class="inside"></div></div>
-                            </li>
-                        </ul>
-                        
-                        <h2 className="mt-3">網紅粉絲需求數</h2>
-                        <ul>
-                            <li>
-                                <input type="radio" id="fans-1" name="selector" className="fans-filter" data-dropdown-value="100~500" />
-                                <label for="fans-1">微量粉絲數</label>
-                                <div class="check"></div>
-                            </li>
-                            <li>
-                                <input type="radio" id="fans-2" name="selector" className="fans-filter" data-dropdown-value="500~1000" />
-                                <label for="fans-2">輕量粉絲數</label>
-                                <div class="check"><div class="inside"></div></div>
-                            </li>
-                            <li>
-                                <input type="radio" id="fans-3" name="selector" className="fans-filter" data-dropdown-value="1000~2000" />
-                                <label for="fans-3">中量級粉絲數</label>
-                                <div class="check"><div class="inside"></div></div>
-                            </li>
-                            <li>
-                                <input type="radio" id="fans-4" name="selector" className="fans-filter" data-dropdown-value="2000~5000" />
-                                <label for="fans-4">重量級粉絲數</label>
-                                <div class="check"><div class="inside"></div></div>
-                            </li>
-                        </ul>
-                        
-                        <h2 className="mt-3">案子薪水需求</h2>
-                        <ul>
-                            <li>
-                                <input type="radio" id="pay-1" name="selector" className="pay-filter" data-dropdown-value="1,000-3,000元" />
-                                <label for="pay-1">要求低薪</label>
-                                <div class="check"></div>
-                            </li>
-                            <li>
-                                <input type="radio" id="pay-2" name="selector" className="pay-filter" data-dropdown-value="3,000-5,000元" />
-                                <label for="pay-2">剛好就好</label>
-                                <div class="check"><div class="inside"></div></div>
-                            </li>
-                            <li>
-                                <input type="radio" id="pay-3" name="selector" className="pay-filter" data-dropdown-value="10,000-50,000元" />
-                                <label for="pay-3">我要高薪</label>
-                                <div class="check"><div class="inside"></div></div>
-                            </li>
-                        </ul>
-                    </div>
+                        <div class="filter">
+                            <h2>網紅需求數</h2>
+                            <ul>
+                                <li>
+                                    <input type="radio" id="people-1" name="selector" className="people-filter" data-dropdown-value="1" />
+                                    <label for="people-1">需求人數：1</label>
+                                    <div class="check"></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="people-2" name="selector" className="people-filter" data-dropdown-value="2" />
+                                    <label for="people-2">需求人數：2</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="people-3" name="selector" className="people-filter" data-dropdown-value="3" />
+                                    <label for="people-3">需求人數：3</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="people-4" name="selector" className="people-filter" data-dropdown-value="3人以上" />
+                                    <label for="people-4">需求人數：3人以上</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                            </ul>
+                            
+                            <h2 className="mt-3">網紅粉絲需求數</h2>
+                            <ul>
+                                <li>
+                                    <input type="radio" id="fans-1" name="selector" className="fans-filter" data-dropdown-value="100~500" />
+                                    <label for="fans-1">微量粉絲數</label>
+                                    <div class="check"></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="fans-2" name="selector" className="fans-filter" data-dropdown-value="500~1000" />
+                                    <label for="fans-2">輕量粉絲數</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="fans-3" name="selector" className="fans-filter" data-dropdown-value="1000~2000" />
+                                    <label for="fans-3">中量級粉絲數</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="fans-4" name="selector" className="fans-filter" data-dropdown-value="2000~5000" />
+                                    <label for="fans-4">重量級粉絲數</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                            </ul>
+                            
+                            <h2 className="mt-3">案子薪水需求</h2>
+                            <ul>
+                                <li>
+                                    <input type="radio" id="pay-1" name="selector" className="pay-filter" data-dropdown-value="1,000-3,000元" />
+                                    <label for="pay-1">要求低薪</label>
+                                    <div class="check"></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="pay-2" name="selector" className="pay-filter" data-dropdown-value="3,000-5,000元" />
+                                    <label for="pay-2">剛好就好</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                                <li>
+                                    <input type="radio" id="pay-3" name="selector" className="pay-filter" data-dropdown-value="10,000-50,000元" />
+                                    <label for="pay-3">我要高薪</label>
+                                    <div class="check"><div class="inside"></div></div>
+                                </li>
+                            </ul>
+                        </div>
                         {/* <div>
                             <ul>
                                 <li className="people-filter" data-dropdown-value="1">需求人數:1</li>
